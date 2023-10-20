8ef7efb3b183622b658d0bb60cd250cdffcb8339
shaik Asif <smd.20sa@gmail.com>
2023-10-20 10:15:14 +0530

    intern

---
+++ src/shopping.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./shopping.css";
+const Shopping = () => {
+  return (
+    <div className="main">
+      <div className="primary">
+        <div className="heading">
+          <h1>productsearch</h1>
+        </div>
+        <div className="search">
+          <input type="search" />
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Shopping;
